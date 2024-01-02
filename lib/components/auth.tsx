0f4234ff985ec990652d142cce9388d502cae6a1
Paul Walsh <paul@walsh.co.il>
2024-01-02 10:50:58 +0200

    Initial commit

---
+++ lib/components/auth.tsx
@@ -0,0 +1,10 @@
+import { createClient } from '@/lib/supabase/server';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+
+export async function AuthAction() {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const { data: { user } } = await supabase.auth.getUser();
+  return (user ? <>hey {user.email} :) <Link href="/logout">Logout</Link></> : <Link href='/login'>Login</Link>)
+}
